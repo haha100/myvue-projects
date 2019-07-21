a8fecbc29a8aa531699c33d127297c511a7256de
isfamous <1150081635@qq.com>
2019-07-21 22:48:18 +0800

    add a project the name is calendar

---
+++ vue-projects/calendar/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <calendar></calendar>
+  </div>
+</template>
+
+<script>
+  import Calendar from "./components/Calendar.vue";
+
+  export default {
+    components: {
+      calendar: Calendar
+    }
+  }
+</script>
